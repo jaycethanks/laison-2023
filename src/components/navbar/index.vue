2840474c0e48448b852f7623ccfc11d5661d82f4
jayce <jaycethanks@outlook.com>
2023-08-14 13:54:22 +0800

    chore: replate main logo file

--- src/components/navbar/index.vue
+++ src/components/navbar/index.vue
@@ -2,15 +2,16 @@
   <div class="navbar">
     <div class="left-side">
       <a-space>
-        <img
+        <!-- <img
           alt="logo"
           src="//p3-armor.byteimg.com/tos-cn-i-49unhts6dw/dfdba5317c0c20ce20e64fac803d52bc.svg~tplv-49unhts6dw-image.image"
-        />
+        /> -->
+        <Logo />
         <a-typography-title
           :style="{ margin: 0, fontSize: '18px' }"
           :heading="5"
         >
-          Arco Pro
+          Laison Tech
         </a-typography-title>
         <icon-menu-fold
           v-if="!topMenu && appStore.device === 'mobile'"
@@ -205,6 +206,7 @@
     UserOutlined,
     LogoutOutlined,
   } from '@ant-design/icons-vue';
+  import Logo from '@/assets/logo.svg';
   import { useDark, useToggle, useFullscreen } from '@vueuse/core';
   import { useAppStore, useUserStore } from '@/store';
   import { LOCALE_OPTIONS } from '@/locale';
