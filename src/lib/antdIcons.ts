7c92a090252458a2b7cd1aae1603456ba49c8d47
jayce <jaycethanks@outlook.com>
2023-08-15 16:23:04 +0800

    chore: before eslint autofix all

--- src/lib/antdIcons.ts
+++ src/lib/antdIcons.ts
@@ -1,2 +1,13 @@
-import { createApp } from 'vue';
-import * as antIcons from '@ant-design/icons-vue'; // 引入ant icons
+import type { App } from 'vue';
+import * as antIcons from '@ant-design/icons-vue';
+
+// 引入ant icons
+// 全局使用图标，遍历引入
+
+export default {
+  install: (app: App<any>) => {
+    for (const i in antIcons) {
+      app.component(i, (antIcons as any)[i]);
+    }
+  },
+};
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,19 @@
 import { createApp } from 'vue';
+
 // import ArcoVue from '@arco-design/web-vue';
 import Antdv from 'ant-design-vue';
 import 'ant-design-vue/dist/reset.css';
+
 // import '@arco-design/web-vue/dist/arco.css';
 // import ArcoVueIcon from '@arco-design/web-vue/es/icon';
-import globalComponents from '@/components';
+import App from './App.vue';
 import router from './router';
 import store from './store';
 import i18n from './locale';
 import directive from './directive';
+import globalComponents from '@/components';
 import './mock';
-import App from './App.vue';
+
 // Styles are imported via arco-plugin. See config/plugin/arcoStyleImport.ts in the directory for details
 // 样式通过 arco-plugin 插件导入。详见目录文件 config/plugin/arcoStyleImport.ts
 // https://arco.design/docs/designlab/use-theme-package
