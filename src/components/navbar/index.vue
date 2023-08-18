62002767dbaf8cbd7a889240d156f76f352aa56b
jayce <jaycethanks@outlook.com>
2023-08-18 13:56:40 +0800

    feat: rewrite navbar element

--- src/components/navbar/index.vue
+++ src/components/navbar/index.vue
@@ -1,49 +1,40 @@
 <template>
-  <div class="navbar">
+  <LayoutHeader :style="layoutHeaderStyles">
     <div class="left-side">
-      <a-space>
-        <!-- <img
-          alt="logo"
-          src="//p3-armor.byteimg.com/tos-cn-i-49unhts6dw/dfdba5317c0c20ce20e64fac803d52bc.svg~tplv-49unhts6dw-image.image"
-        /> -->
-        <Logo />
-        <a-typography-title
-          :style="{ margin: 0, fontSize: '18px' }"
-          :heading="5"
-        >
-          Laison Tech
-        </a-typography-title>
-        <MenuFoldOutlined
-          v-if="!topMenu && appStore.device === 'mobile'"
-          style="font-size: 22px; cursor: pointer"
-          @click="toggleDrawerMenu"
-        />
-      </a-space>
+      <Logo />
+      <a-typography-title :style="{ fontSize: '18px' }" :heading="5">
+        Laison
+      </a-typography-title>
+      <MenuFoldOutlined
+        v-if="!topMenu && appStore.device === 'mobile'"
+        style="font-size: 22px; cursor: pointer"
+        @click="toggleDrawerMenu"
+      />
     </div>
     <div class="center-side">
       <Menu v-if="topMenu" />
     </div>
-    <ul class="right-side">
-      <li>
-        <a-tooltip :content="$t('settings.search')">
-          <a-button
-            class="nav-btn"
-            type="outline"
-            shape="circle"
-            :icon="h(SearchOutlined)"
-          >
+    <a-menu
+      :selectable="false"
+      class="right-side"
+      :style="menuStyles"
+      mode="horizontal"
+    >
+      <a-menu-item>
+        <a-tooltip :title="$t('settings.search')">
+          <a-button class="nav-btn" type="outline" :icon="h(SearchOutlined)">
             <template #icon>
               <SearchOutlined />
             </template>
           </a-button>
         </a-tooltip>
-      </li>
-      <li>
-        <a-tooltip :content="$t('settings.language')">
+      </a-menu-item>
+
+      <a-menu-item>
+        <a-tooltip :title="$t('settings.language')">
           <a-button
             class="nav-btn"
             type="outline"
-            shape="circle"
             :icon="h(GlobalOutlined)"
             @click="setDropDownVisible"
           />
@@ -59,10 +50,10 @@
             </a-menu>
           </template>
         </a-dropdown>
-      </li>
-      <li>
+      </a-menu-item>
+      <a-menu-item style="display:flex;justify-content: center;align-items: center;">
         <a-tooltip
-          :content="
+          :title="
             theme === 'light'
               ? $t('settings.navbar.theme.toDark')
               : $t('settings.navbar.theme.toLight')
@@ -71,7 +62,7 @@
           <a-button
             class="nav-btn"
             type="outline"
-            shape="circle"
+            style="display:flex;justify-content: center;align-items: center;"
             @click="handleToggleTheme"
           >
             <template #icon>
@@ -80,15 +71,14 @@
             </template>
           </a-button>
         </a-tooltip>
-      </li>
-      <li>
-        <a-tooltip :content="$t('settings.navbar.alerts')">
+      </a-menu-item>
+      <a-menu-item>
+        <a-tooltip :title="$t('settings.navbar.alerts')">
           <div class="message-box-trigger">
             <a-badge :count="9" dot>
               <a-button
                 class="nav-btn"
                 type="outline"
-                shape="circle"
                 :icon="h(BellOutlined)"
                 @click="setPopoverVisible"
               />
@@ -106,10 +96,10 @@
             <MessageBox />
           </template>
         </a-popover>
-      </li>
-      <li>
+      </a-menu-item>
+      <a-menu-item>
         <a-tooltip
-          :content="
+          :title="
             isFullscreen
               ? $t('settings.navbar.screen.toExit')
               : $t('settings.navbar.screen.toFull')
@@ -118,24 +108,22 @@
           <a-button
             class="nav-btn"
             type="outline"
-            shape="circle"
             :icon="isFullscreen ? h(CompressOutlined) : h(ExpandOutlined)"
             @click="toggleFullScreen"
           />
         </a-tooltip>
-      </li>
-      <li>
-        <a-tooltip :content="$t('settings.title')">
+      </a-menu-item>
+      <a-menu-item>
+        <a-tooltip :title="$t('settings.title')">
           <a-button
             class="nav-btn"
             type="outline"
-            shape="circle"
             :icon="h(SettingOutlined)"
             @click="setVisible"
           />
         </a-tooltip>
-      </li>
-      <li>
+      </a-menu-item>
+      <a-menu-item>
         <a-dropdown trigger="click">
           <a-avatar
             :size="32"
@@ -180,14 +168,19 @@
             </a-menu>
           </template>
         </a-dropdown>
-      </li>
-    </ul>
-  </div>
+      </a-menu-item>
+    </a-menu>
+  </LayoutHeader>
 </template>
 
 <script lang="ts" setup>
 import { computed, h, inject, ref } from 'vue';
-import { message as Message } from 'ant-design-vue';
+import {
+  LayoutHeader,
+  message as Message,
+  theme as antTheme,
+} from 'ant-design-vue';
+
 import {
   BellOutlined,
   CheckSquareOutlined,
@@ -223,6 +216,23 @@ const avatar = computed(() => {
 });
 const theme = computed(() => {
   return appStore.theme;
+});
+const layoutHeaderStyles = computed(() => {
+  const { token } = antTheme.useToken();
+  const colors = { backgroundColor: token.value.colorBgLayout };
+  const layout = {
+    position: 'fixed',
+    zIndex: 100,
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'space-between',
+    padding: 0,
+  };
+  return { ...colors, ...layout };
+});
+const menuStyles = computed(() => {
+  const { token } = antTheme.useToken();
+  return { backgroundColor: token.value.colorBgLayout };
 });
 const topMenu = computed(() => appStore.topMenu && appStore.menu);
 const isDark = useDark({
@@ -276,57 +286,65 @@ const toggleDrawerMenu = inject('toggleDrawerMenu') as () => void;
     display: flex;
     justify-content: space-between;
     height: 100%;
-    background-color: var(--color-bg-2);
+    // background-color: var(--color-bg-2);
     border-bottom: 1px solid var(--color-border);
   }
 
   .left-side {
     display: flex;
+    gap: 1em;
     align-items: center;
     padding-left: 20px;
   }
 
-  .center-side {
-    flex: 1;
-  }
+  // .center-side {
+  //   flex: 1;
+  // }
 
-  .right-side {
-    display: flex;
-    padding-right: 20px;
-    list-style: none;
-    :deep(.locale-select) {
-      border-radius: 20px;
-    }
-    li {
-      display: flex;
-      align-items: center;
-      padding: 0 10px;
-    }
+  // .right-side {
+  //   display: flex;
+  //   padding-right: 20px;
+  //   list-style: none;
+  //   :deep(.locale-select) {
+  //     border-radius: 20px;
+  //   }
+  //   li {
+  //     display: flex;
+  //     align-items: center;
+  //     padding: 0 10px;
+  //   }
 
-    a {
-      color: var(--color-text-1);
-      text-decoration: none;
+  //   a {
+  //     color: var(--color-text-1);
+  //     text-decoration: none;
+  //   }
+  .nav-btn {
+    &:hover {
+      background-color: #b8b8b869;
     }
-    .nav-btn {
-      border-color: rgb(var(--gray-2));
-      color: rgb(var(--gray-8));
-      font-size: 16px;
-    }
-    .trigger-btn,
-    .ref-btn {
-      position: absolute;
-      bottom: 14px;
-    }
-    .trigger-btn {
-      margin-left: 14px;
-    }
+    font-size: 16px;
   }
+  //   .trigger-btn,
+  //   .ref-btn {
+  //     position: absolute;
+  //     bottom: 14px;
+  //   }
+  //   .trigger-btn {
+  //     margin-left: 14px;
+  //   }
+  // }
 </style>
 
 <style lang="less">
   .message-popover {
     .arco-popover-content {
-      margin-top: 0;
+      // margin-top: 0;
     }
+  }
+</style>
+
+<style scoped lang="less">
+  :deep(.ant-menu-horizontal) > .ant-menu-item::after {
+    display: none !important;
   }
 </style>
