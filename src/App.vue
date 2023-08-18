2cba22a16635ae26c3d38607da458d0aa359daf1
jayce <jaycethanks@outlook.com>
2023-08-18 14:27:28 +0800

    feat: support provider theme switch

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <a-config-provider
     :locale="locale"
     :theme="{
-      // algorithm: theme.darkAlgorithm,
+      algorithm: isDark ? theme.darkAlgorithm : null,
       token: {
         colorPrimary: '#FA541C',
       },
@@ -16,11 +16,14 @@
 <script lang="ts" setup>
 import { computed } from 'vue';
 
-// import { theme } from 'ant-design-vue';
+import { theme } from 'ant-design-vue';
 import enUS from 'ant-design-vue/es/locale/en_US';
 import zhCN from 'ant-design-vue/es/locale/zh_CN';
+import useThemes from '@/hooks/themes';
 import GlobalSetting from '@/components/global-setting/index.vue';
 import useLocale from '@/hooks/locale';
+
+const { isDark } = useThemes();
 
 const { currentLocale } = useLocale();
 const locale = computed(() => {
